c6c47c9058de3f057adac3b7410fbc2604d1246f
Алексей Фетисов <alfet123@gmail.com>
2015-12-14 21:45:04 +0500

    Форма - отправка формы с помощью ajax

--- js/script.js
+++ js/script.js
@@ -41,4 +41,65 @@
     };
   });
 
+  if (!("FormData" in window)) {
+    return;
+  }
+
+  var form = document.querySelector(".review");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var data = new FormData(form);
+
+    request(data, function(response) {
+      console.log(response);
+    });
+  });
+
+  function request(data, fn) {
+    var xhr = new XMLHttpRequest();
+    var time = (new Date()).getTime();
+
+    xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time);
+
+    xhr.addEventListener("readystatechange", function() {
+      if (xhr.readyState == 4) {
+        fn(xhr.responseText);
+      }
+    });
+
+    xhr.send(data);
+  }
+
+  if ("FileReader" in window) {
+    var area = document.querySelector(".photo__list");
+
+    form.querySelector("#select-file").addEventListener("change", function() {
+      var files = this.files;
+
+      for (var i = 0; i < files.length; i++) {
+        preview(files[i]);
+      }
+    });
+
+    function preview(file) {
+      if (file.type.match(/image.*/)) {
+        var reader = new FileReader();
+
+        reader.addEventListener("load", function(event) {
+          var img = document.createElement("img");
+
+          img.src = event.target.result;
+          img.alt = file.name;
+
+          area.appendChild(img);
+        });
+
+        reader.readAsDataURL(file);
+      }
+    }
+
+  }
+
 })();
